35dcf969267c2fc28dcb09a7416795341412c34a
CaptainCthulhu <theonegod@gmail.com>
2020-05-08 20:11:15 -0600

    Changed things around.

--- ClientApp/src/components/Grid.tsx
+++ ClientApp/src/components/Grid.tsx
@@ -2,21 +2,24 @@ import React, { Component } from "react";
 import GridElement from "./GridElement";
 import Table from "react-bootstrap/Table";
 
-
 export class Grid extends Component {
-  static displayName = Grid.name;
-
   render() {
-    var elems = [];    
+    let elems = [];
     let count = 1;
-    for (var y = 1; y < 10; y++) {      
+
+    for (var y = 1; y < 10; y++) {
       var row = [];
-      for (var x = 1; x < 10; x++) {        
-        row.push(<td key={x*y}><GridElement xLocation={x} yLocation={y} value={count} /></td>);        
+      for (var x = 1; x < 10; x++) {
+        row.push(
+          <td key={x * y}>
+            <GridElement xLocation={x} yLocation={y} value={count} />
+          </td>
+        );
         count++;
-      }    
-      elems.push(<tr key={y} >{row}</tr>);
+      }
+      elems.push(<tr key={y}>{row}</tr>);
     }
+
     return (
       <div>
         <Table striped bordered hover variant="dark">
